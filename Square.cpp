0680fb51c5df78e0625cf40674a2d42713ae99de
MarkKlimov <klimov.mark87@gmail.com>
2020-06-07 02:15:56 +0700

    added moving methods

--- Square.cpp
+++ Square.cpp
@@ -1,6 +1,40 @@
 #include "Square.h"
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
+void Square::delete_zero(std::vector<double>& temp)
+{
+	temp.erase(std::remove(temp.begin(), temp.end(), 0), temp.end());
+}
+
+void Square::summ(std::vector<double>& temp)
+{
+	for (int k = 0; k < temp.size() - 1; k++)
+	{
+		if (temp[k] == temp[k + 1])
+		{
+			temp[k] = temp[k] * 2;
+			temp[k + 1] = 0;
+		}
+	}
+}
+
+void Square::erase_column(int column)
+{
+	for (int i = 0; i < n; i++)
+	{
+		data[i][column] = 0;
+	}
+}
+
+void Square::erase_row(int row)
+{
+	for (int i =0; i < n; i++)
+	{
+		data[row][i] = 0;
+	}
+}
 
 Square::Square()
 {
@@ -25,22 +59,10 @@ Square::Square(int N)
 		addRandom();
 }
 
-void Square::show()
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			std::cout << data[i][j] << "\t";
-		}
-		std::cout << std::endl;
-	}
-}
-
 void Square::addRandom()
 {
 	bool i = false;
-	while (i == false)
+	while (i == false) 
 	{
 		int x = rand() % n;
 		int y = rand() % n;
@@ -51,3 +73,129 @@ void Square::addRandom()
 		}
 	}
 }
+
+void Square::move_right()
+{
+	std::vector<double> temp;
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			temp.push_back(data[i][k]);
+		}
+		delete_zero(temp);
+		if (temp.size() > 0)
+		{
+			std::reverse(temp.begin(), temp.end());
+			summ(temp);
+			delete_zero(temp);
+			temp.resize(n);
+			std::reverse(temp.begin(), temp.end());
+			erase_row(i);
+			for (int k = 0; k < temp.size(); k++)
+			{
+				data[i][k] = temp[k];
+			}
+		}
+		temp.clear();
+	}
+	for (int i = 0; i < n / 2; i++)
+	{
+		addRandom();
+	}
+
+}
+
+void Square::move_left()
+{
+	std::vector<double> temp;
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0;	k < n; k++)
+		{
+			temp.push_back(data[i][k]);		
+		}
+		delete_zero(temp);
+		if (temp.size() > 0)
+		{
+			summ(temp);
+			delete_zero(temp);
+			erase_row(i);
+			for (int k = 0; k < temp.size(); k++)
+			{
+				data[i][k] = temp[k];
+			}
+		}
+		temp.clear();
+	}
+	for (int i = 0; i < n/2; i++)
+	{
+		addRandom();
+	}
+
+}
+
+void Square::move_down()
+{
+	std::vector<double> temp;
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			temp.push_back(data[k][i]);
+		}
+		delete_zero(temp);
+		if (temp.size() > 0)
+		{
+			std::reverse(temp.begin(), temp.end());
+			summ(temp);
+			delete_zero(temp);
+			temp.resize(n);
+			std::reverse(temp.begin(), temp.end());
+			erase_column(i);
+			for (int k = 0; k < temp.size(); k++)
+			{
+				data[k][i] = temp[k];
+			}
+		}
+		temp.clear();
+	}
+	for (int i = 0; i < n / 2; i++)
+	{
+		addRandom();
+	}
+}
+
+void Square::move_up()
+{
+	std::vector<double> temp;
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			temp.push_back(data[k][i]);
+		}
+		delete_zero(temp);
+		if (temp.size() > 0)
+		{
+			summ(temp);
+			delete_zero(temp);
+			erase_column(i);
+			for (int k = 0; k < temp.size(); k++)
+			{
+				data[k][i] = temp[k];
+			}
+		}
+		temp.clear();
+	}
+	for (int i = 0; i < n / 2; i++)
+	{
+		addRandom();
+	}
+}
+
+int Square::getItem(int i, int k)
+{
+	return data[i][k];
+}
+
--- Square.h
+++ Square.h
@@ -1,18 +1,27 @@
 #pragma once
 #include <stdlib.h>
+#include<vector>
+
 class Square
 {
 private:
 	int n;
 	double** data;
+	void delete_zero(std::vector<double>& temp);
+	void summ(std::vector<double>& temp);
+	void erase_column(int);// забить столбец нулями
+	void erase_row(int);// забить строку нулями
 public:
 	Square();
 	Square(int);
 
-	void show();
 	bool canGame();
-	void move(int);//1 for UP, 2 for DOWN, 3 for LEFT, 4 for Right
-	void removeZero(double[],int); //1 for UP, 2 for DOWN, 3 for LEFT, 4 for Right
-	void summ(double[], int); //1 for UP, 2 for DOWN, 3 for LEFT, 4 for Right
 	void addRandom();
+	
+	void move_right();
+	void move_left();
+	void move_down();
+	void move_up();
+
+	int getItem(int, int);
 };
